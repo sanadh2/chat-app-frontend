0164e11e2bf5ae04e523417b0273d896af2ef2b3
sanadh <suriyaakku@gmail.com>
2025-03-19 13:27:31 +0530

    Create useConversations.tsx

---
+++ src/hooks/useConversations.tsx
@@ -0,0 +1,19 @@
+import { useQuery } from "@tanstack/react-query";
+import { GroupType, UserType } from "../types/models";
+import axiosInstance from "../utils/axiosInstance";
+
+const fetchConversations = async (): Promise<{
+  success: boolean;
+  users: UserType[];
+  groups: GroupType[];
+}> => {
+  const res = await axiosInstance.get("/chat/conversations");
+  return res.data;
+};
+
+export default function useConversations() {
+  return useQuery({
+    queryKey: ["users"],
+    queryFn: fetchConversations,
+  });
+}
